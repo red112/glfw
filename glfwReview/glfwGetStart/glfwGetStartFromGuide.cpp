e867984ae1cf88489c81031e261bd81c2142f1f6
Wonil Shim <wonil.shim.kt@gmail.com>
2023-04-07 09:00:10 +0900

    Draw triangle

--- glfwReview/glfwGetStart/glfwGetStartFromGuide.cpp
+++ glfwReview/glfwGetStart/glfwGetStartFromGuide.cpp
@@ -1,7 +1,53 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-void main()
+int main(void)
 {
-	std::cout << "Hello, glfw";
+    GLFWwindow* window;
 
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+
+    glClearColor(1, 0, 0,1);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        glViewport(0, 0, 640, 480);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glColor3f(0.f, 1.f, 0.f);
+        glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5f, -0.5f, 0.f);
+        glVertex3f( 0.5f, -0.5f, 0.f);
+        glVertex3f( 0.f,   0.5f, 0.f);
+        glEnd();
+
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
